4bf8ceef5efebd73f850c7bf58bda700ea32cedc
Danyga2020 <106919621+Danyga2020@users.noreply.github.com>
2022-06-05 19:25:03 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "0c780f6db3124fafbdc3dcde437e6f21"
 api_id = 16536294
 bot_token = "5418447536:AAHEy9Bk3jjA3PhyFOnSrN8dgNHlyrD2xpQ"
-#bot_admin_group = -1001505005021
+bot_admin_group = -792402445
